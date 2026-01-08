72947e244ad5e881e3d76f7c661caaaa829ca62e
Deepak-Palpandi-Developer <deepakpalpandi.work@gmail.com>
2026-01-08 21:31:28 +0530

    feat: add authentication components for login and reset password functionality

---
+++ src/app/features/auth/components/login/login.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'heart-beat-login',
+  imports: [],
+  templateUrl: './login.component.html',
+  styles: ``,
+})
+export class LoginComponent {
+
+}
