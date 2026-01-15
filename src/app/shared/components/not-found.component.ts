840ba8cdf3f13934668fe2b769b90ce2b4a4f3a0
Deepak-Palpandi-Developer <deepakpalpandi.work@gmail.com>
2026-01-15 23:10:46 +0530

    feat: Add NotFoundComponent for handling 404 errors

---
+++ src/app/shared/components/not-found.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'heart-beat-not-found',
+  imports: [],
+  template: `
+    <p>
+      not-found works!
+    </p>
+  `,
+  styles: ``,
+})
+export class NotFoundComponent {
+
+}
