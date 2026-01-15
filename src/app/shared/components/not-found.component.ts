49d639c2fe2be19959e42411c38606d3edba7f14
Deepak-Palpandi-Developer <deepakpalpandi.work@gmail.com>
2026-01-15 23:15:41 +0530

    fix: update copyright notice in NotFoundComponent

--- src/app/shared/components/not-found.component.ts
+++ src/app/shared/components/not-found.component.ts
@@ -33,7 +33,7 @@ import { GridShapeComponent } from './grid-shape.component';
       <p
         class="absolute text-sm text-center text-gray-500 -translate-x-1/2 bottom-6 left-1/2 dark:text-gray-400"
       >
-        &copy; {{ currentYear }} - TailAdmin
+        &copy; {{ currentYear }} - HeartBeat. All rights reserved.
       </p>
     </div>
   `,
